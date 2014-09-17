98afc64eadcba1b554db8586327862b5d5e102c2
Aresni Nerinovsky <arseny-n@yandex.ru>
2014-09-17 21:31:47 +0400

    first commit

--- atmel-studio/FREERTOS_SAM_LWIP_DHCP_EXAMPLE44/FREERTOS_SAM_LWIP_DHCP_EXAMPLE44/Debug/src/ASF/thirdparty/freertos/demo/lwip_avr32_uc3_example/network/basictftp/BasicTFTP.d
+++
@@ -1,3 +0,0 @@
-src/ASF/thirdparty/freertos/demo/lwip_avr32_uc3_example/network/basictftp/BasicTFTP.d \
- src/ASF/thirdparty/freertos/demo/lwip_avr32_uc3_example/network/basictftp/BasicTFTP.o: \
- ../src/ASF/thirdparty/freertos/demo/lwip_avr32_uc3_example/network/basictftp/BasicTFTP.c
